017f789b530f855a8ff3990ac00033b1b7572d3a
htyte <shtyte@gmail.com>
2020-10-21 13:15:26 -0400

    move index.html file

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.static('src/public'));
 
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/', 'index.html'));
+    res.sendFile(path.resolve('index.html'));
 })
 
 app.post('/email', (req, res) => {

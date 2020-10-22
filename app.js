225ec45737bb4965c5c93c695088943d04a85f1d
htyte <shtyte@gmail.com>
2020-10-21 21:03:37 -0400

    add favicon and fix path

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 app.use(express.static('public'));
 
-
 app.get('/', (req,res) => {
     res.sendFile(path.resolve('index.html'));
 })
